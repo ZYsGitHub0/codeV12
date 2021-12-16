644c3078c6d72eee4659509c0b549664cf270301
zhangyi <709608032@qq.com>
2021-12-16 20:32:57 +0800

    添加注释:springboot启动类启动时参数需要启动类的.class

--- springboot_in/src/main/java/com/happy233/HelloApplication.java
+++ springboot_in/src/main/java/com/happy233/HelloApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication//标识当前类是springboot启动类/引导类
 public class HelloApplication {
     public static void main(String[] args) {
+        //为什么需要启动类.class字节码文件？
+        //Because:利用字节码获取包名 那么springBoot的默认作用域则为该包 会默认为该包下的所有被注解或配置的类IOC创建bean对象
         SpringApplication.run(HelloApplication.class, args);
     }
 }
