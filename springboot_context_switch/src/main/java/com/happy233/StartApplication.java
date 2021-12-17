f5fdcc42c4513de34620fdba45fd966a12393ad1
zhangyi <709608032@qq.com>
2021-12-17 15:18:26 +0800

    springboot 环境切换初始化项目

---
+++ springboot_context_switch/src/main/java/com/happy233/StartApplication.java
@@ -0,0 +1,17 @@
+package com.happy233;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * TODOed:
+ *
+ * @author zy136
+ * @date 2021/12/17 15:08
+ */
+@SpringBootApplication
+public class StartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class, args);
+    }
+}
